124009f9bfe0b017497dacbfdafd8b083816b686
albertodurante <34619112+albertodurante@users.noreply.github.com>
2017-12-28 18:23:53 +0100

    Update RTTnode.java

--- RTTnode.java
+++ RTTnode.java
@@ -12,14 +12,39 @@ public class RTTnode {
     * public static
     */
     public static final String JoinNode = "JoinNode";
+    /**
+     * public 
+     */
     public static final String ForkNode = "ForkNode";
+    /**
+     * public 
+     */
     public static final String BranchNode = "BranchNode";
+    /**
+     * public 
+     */
     public static final String Node = "Node";
+    /**
+     * public 
+     */
     public static final String InitialNode = "InitialNode";
+    /**
+     * public 
+     */
     public static final String FinalNode = "FinalNode";
+    /**
+     * public 
+     */
     public String ontologyClass;
+    /**
+     * public 
+     */
     public String name;
+    /**
+     * public 
+     */
     public String id;
+    
     private String type;
 
     private static int idCounter = 0;
